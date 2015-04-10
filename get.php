6696acb757e187604e6b0aa4f26ab1740dc70991
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2015-04-10 11:28:02 +0700

    auto

--- get.php
+++ get.php
@@ -11,4 +11,5 @@ include('db_util.php');
 
 $id = $_GET['id'];
 $imgUrl = getImg($id);
-echo json_encode(['id' => $id, 'url' => $imgUrl]);
+// echo json_encode(['id' => $id, 'url' => $imgUrl]);
+echo $imgUrl;
